af1d28f26a9d1a84e43cb783ec92cfc54283c085
aden zhang <adenzh@gmailcom>
2018-12-25 20:28:12 -0500

    added main() for unittests UNITTEST_MODE

--- src/app.cpp
+++ src/app.cpp
@@ -1,10 +1,12 @@
 
-#define CATCH_CONFIG_MAIN
-
+//#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
+// define CATCH_CONFIG_RUNNER if main() is supplied
 #include <ftl/catch_or_ignore.h>
 
 #include <algorithm>
 #include <bitset>
+#include <chrono>
 #include <climits>
 #include <cmath>
 #include <cstdio>
@@ -61,3 +63,35 @@ int main_app()
     cout << "m:" << sqrt(-4.5) << endl;
     return 0;
 }
+
+#ifdef UNITTEST_MODE
+
+int main(int argc, char* argv[])
+{
+    //========= RUN C++ TESTS ==================================
+
+    int iRet = 1;
+    {
+        Catch::Session session;
+
+        iRet = session.applyCommandLine(argc, argv);
+
+        if (iRet == 0) {
+            session.configData().shouldDebugBreak = true;
+            session.configData().showDurations = Catch::ShowDurations::Always;
+
+            const auto start = std::chrono::steady_clock::now();
+            iRet = session.run();
+            const auto stop = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+            std::printf("\n===============================================================================\n"
+                        "Tests took %ld us\n"
+                        "===============================================================================\n\n",
+                duration.count());
+        }
+    }
+
+    return iRet;
+}
+
+#endif
