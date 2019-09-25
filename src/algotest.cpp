73bf54a177b2ba0a0711864396d70aaaa3eaaefa
aden zhang <adenzh@gmailcom>
2019-09-24 20:55:28 -0400

    updated ftl

--- src/algotest.cpp
+++ src/algotest.cpp
@@ -1,16 +1,20 @@
 #include <ftl/ftl.h>
 #include <ftl/enum_create.h>
 #include <list>
+#include <iostream>
 
 using namespace std;
+
 
 class Solution
 {
 public:
 };
 
+using namespace ftl;
+
+
 TEST_FUNC( tmp_tests )
 {
-    using namespace ftl::serialization;
     // Solution sln;
 }
